508a85d0ae8c92e1ca6755aaa8243b5a449e0702
student <nitish.seceon@gmail.com>
2022-02-12 14:46:15 +0000

    First Commit for My Final Assignment

---
+++ src/main/java/com/assignment/nitishfinalassignment/NitishfinalassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.nitishfinalassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitishfinalassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NitishfinalassignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/nitishfinalassignment/NitishfinalassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.nitishfinalassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NitishfinalassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
